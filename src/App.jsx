a31c60abb1d48630e4f2adf2f4a99f7a56016b66
dongyoon7212 <dongyoon7212@naver.com>
2025-12-23 16:04:27 +0900

    Feat: react 끝

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Emotion1 from "./Emotion/Emotion1";
 import FruitsFilter from "./ReactStudy/LifeCycle/FruitsFilter";
@@ -18,38 +19,50 @@ import ReactHook07 from "./ReactStudy/ReactHook07";
 import ReactHook08 from "./ReactStudy/ReactHook08";
 import ReactHook09 from "./ReactStudy/ReactHook09";
 import Index from "./TodoList/pages";
+import Router01 from "./RouterStudy/Router01/Router01";
+import Router02 from "./RouterStudy/Router02/Router02";
+import Router03 from "./RouterStudy/Router03/Router03";
+import Router04 from "./RouterStudy/Router04/Router04";
 
 function App() {
     const name = "이동윤";
     const age = 27;
     return (
         <>
-            {/* <React01 /> */}
-            {/* <React02 /> */}
-            {/* <React03
+            <BrowserRouter>
+                {/* <React01 /> */}
+                {/* <React02 /> */}
+                {/* <React03
                 name={name}
                 age={age}
                 address={"부산시"}
                 tel={"010-1234-5678"}
             /> */}
-            {/* <React04>
+                {/* <React04>
                 <h1>자식요소</h1>
             </React04> */}
-            {/* <ReactHook01 /> */}
-            {/* <CountState /> */}
-            {/* <ReactHook03 /> */}
-            {/* <ReactHook04 /> */}
-            {/* <ReactHook05 /> */}
-            {/* <ReactHook06 /> */}
-            {/* <ReactHook07 /> */}
-            {/* <Render /> */}
-            {/* <LifeCycleApp /> */}
-            {/* <FruitsFilter /> */}
-            {/* <SignupForm /> */}
-            {/* <ReactHook08 /> */}
-            {/* <Emotion1 /> */}
-            {/* <ReactHook09 /> */}
-            <Index />
+                {/* <ReactHook01 /> */}
+                {/* <CountState /> */}
+                {/* <ReactHook03 /> */}
+                {/* <ReactHook04 /> */}
+                {/* <ReactHook05 /> */}
+                {/* <ReactHook06 /> */}
+                {/* <ReactHook07 /> */}
+                {/* <Render /> */}
+                {/* <LifeCycleApp /> */}
+                {/* <FruitsFilter /> */}
+                {/* <SignupForm /> */}
+                {/* <ReactHook08 /> */}
+                {/* <Emotion1 /> */}
+                {/* <ReactHook09 /> */}
+                {/* <Index /> */}
+                {/* <Router01 /> */}
+                {/* <Router02 /> */}
+                {/* <Router03 /> */}
+                <Router04 />
+            </BrowserRouter>
+            {/* BrowserRouter로 감싸진 내부에 있는 컴포넌트들만 Routes, Route, Link, useNavigation같은
+            라우터 기능을 쓸 수 있다. */}
         </>
     );
 }
